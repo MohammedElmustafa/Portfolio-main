04ca15cca6c575fafe88bf9a84f67f0624e06421
MohammedElmustafa <mohamed.elmustafa@icloud.com>
2025-06-18 16:17:33 +0300

    V1.0.7

--- src/app/(main)/blog/page.js
+++ src/app/(main)/blog/page.js
@@ -13,9 +13,7 @@ const Page = () => {
             <h1 className='text-[44px] font-[600] max-[480px]:text-[32px]'>
               {`Today's`} <span className='text-[#755BB4]'>Blog</span>
             </h1>
-            <p className='text-[14px] dark:text-white text-black font-[500] text-center max-[480px]:text-justify'>
-              {`A blog, short for "weblog," is a regularly updated website or online platform where individuals, organizations, or groups publish and share written, multimedia, or interactive content on various topics. Blogs typically feature entries or posts presented in reverse chronological order, with the most recent content appearing at the top.`}
-            </p>
+            <br/>
             <div className='w-full grid grid-cols-3 gap-4 max-[480px]:grid-cols-1 mt-4'>
               {Array.isArray(blogAllData) &&
                 blogAllData.map((item) => {
